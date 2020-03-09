dc5168c5c24628379f12589084b5633bc142ab8d
Andrew Palmer <atp@sdf.org>
2020-03-09 17:35:14 -0500

    Add click dependency

--- polling/cli.py
+++ polling/cli.py
@@ -1,4 +1,5 @@
 import json
+import click
 from .clients import HuffpoClient
 
 
@@ -6,6 +7,7 @@ def pretty_dumps(data):
     return json.dumps(data, indent=2)
 
 
+@click.command()
 def main():
     client = HuffpoClient()
     result = client.polls()
