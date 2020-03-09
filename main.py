2c5f0b1517ecfc257434230551b7b8aef19b3bab
Andrew Palmer <atp@sdf.org>
2020-03-09 17:22:29 -0500

    Create HuffpoClient

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import json
 import requests
 
+
 def url(path):
     return f'https://elections.huffingtonpost.com/pollster/api/v2/{path}'
 
-def dumps(data):
+
+class HuffpoClient(object):
+    def polls(self):
+        response = requests.get(url('polls'))
+        response.raise_for_status()
+        return response.json()
+
+
+def pretty_dumps(data):
     return json.dumps(data, indent=2)
 
+
 def main():
-    response = requests.get(url('polls'))
-    print(dumps(response.json()))
+    client = HuffpoClient()
+    result = client.polls()
+    print(pretty_dumps(result))
+
 
 if __name__ == '__main__':
     main()
