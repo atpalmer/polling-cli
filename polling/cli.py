7e1409434d7d1ae5e0ee6ed878249b9adea1027e
Andrew Palmer <atp@sdf.org>
2020-03-09 17:38:39 -0500

    Add click groups

--- polling/cli.py
+++ polling/cli.py
@@ -7,8 +7,18 @@ def pretty_dumps(data):
     return json.dumps(data, indent=2)
 
 
-@click.command()
+@click.group()
 def main():
+    pass
+
+
+@main.group()
+def polls():
+    pass
+
+
+@polls.command()
+def raw():
     client = HuffpoClient()
     result = client.polls()
     print(pretty_dumps(result))
